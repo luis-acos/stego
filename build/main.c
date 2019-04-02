9db16c843065cc98ab62b46cd01ca80e2e582b58
ecimperative <46550718+ecimperative@users.noreply.github.com>
2019-04-02 18:41:34 -0400

    Update main.c

--- build/main.c
+++ build/main.c
@@ -5,7 +5,7 @@
 #include "stego.h"
 #include <pthread.h>
 
-#DEFINE NUM_THREADS 4 
+#define NUM_THREADS 4 
 
 pthread thread_store[NUM_THREADS];
 
@@ -18,7 +18,6 @@ void print_help(char *path){
            "-d : Decode text from image\n",
            path, path);
 }
-
 
 int main(int argc, char **argv) {
 
@@ -40,18 +39,18 @@ int main(int argc, char **argv) {
     
     //TODO fork then exec ffmpeg to split up source file into component bmp files 
     
-    //TODO split up text file into portions equivalent to the number of frames 
-    
-    //TODO spin off number of threads to conduct steg operations 
-    
-    //TODO 
-
     if(mode){
+        //TODO split up text file into portions equivalent to the number of frames? or some other form of parsing 
+        //TODO spin off number of threads to conduct encode operation
         encode(argv[2], argv[3], argv[4]);
+        //TODO fork then exec ffmpeg to join files back into a single ouput video (ensure same size and frame rate as source, use lossless codec)
+        //put ffmpeg arguments here, theyre pretty long and detailed 
     } else{
+        //TODO spin off number of threads to conduct decode operation
+        //TODO should output a single text file, figure out how to join multiple threads (mutex that doesnt ruin parrallelization?)
         decode(argv[2], argv[3]);
     }
-
+    
     return EXIT_SUCCESS;
 }
 
