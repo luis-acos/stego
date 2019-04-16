7b464cfdcca6ef94a6b6332ac38adb821d9817e1
ecimperative <46550718+ecimperative@users.noreply.github.com>
2019-04-15 22:38:13 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -23,7 +23,7 @@ int chars_in_text = 399122;
 int chars_per_frame = 51240;
 
 char **text_store;
-char **output_store = {"x00.sws", "x01.sws", "x02.sws", "x03.sws", "x04.sws", "x05.sws", "x06.sws", "x07.sws"};
+char *output_store[] = {"x00.sws", "x01.sws", "x02.sws", "x03.sws", "x04.sws", "x05.sws", "x06.sws", "x07.sws"};
 
 char *frame_store[] = { "frame000000001.bmp", "frame000000002.bmp", "frame000000003.bmp", "frame000000004.bmp", 
                                 "frame000000005.bmp", "frame000000006.bmp", "frame000000007.bmp", "frame000000008.bmp"};
