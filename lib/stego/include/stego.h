986fe562d4fce4d21ca9eb0d5bcb357349aaf777
ecimperative <46550718+ecimperative@users.noreply.github.com>
2019-04-16 20:50:37 -0400

    Update stego.h

--- lib/stego/include/stego.h
+++ lib/stego/include/stego.h
@@ -7,6 +7,16 @@
 
 #include <stdbool.h>
 
+typedef struct _thread_data_t{
+  
+  char *text_source;
+  char *original_image;
+  char *destination_image;
+  char *image_source;
+  char *text_destination;
+  
+} thread_data_t;
+
 void _store_length(long size, unsigned char *data);
 long _get_length(const unsigned char *data);
 void *encode(void* arg);
