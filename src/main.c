b3214a44174d8ad8b002d2c343892525f8656573
ecimperative <46550718+ecimperative@users.noreply.github.com>
2019-04-18 16:57:46 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -163,8 +163,7 @@ void join_ffmpeg (char *output_video)
     {
       printf("Joining frames into encoded video. Time to grab some tea.\n");
     
-      char *instructions[] = {"ffmpeg", "-r", "25", "-s", "1280x720", "-i", "frame%09d.bmp", 
-			      "-c:a", "copy", "-crf", "0", output_video, NULL };
+      char *instructions[] = {"ffmpeg", "-r", "25", "-s", "1280x720", "-i", "frame%09d.bmp", "-c:a", "copy", "-crf", "0", output_video, NULL };
       execvp(FFMPEG_PATH, instructions);
     }
 }
