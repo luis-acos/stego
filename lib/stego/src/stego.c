88a8442361273a6f7a3719035b0160226c2f9d1b
ecimperative <46550718+ecimperative@users.noreply.github.com>
2019-04-18 21:17:35 -0400

    Update stego.c

--- lib/stego/src/stego.c
+++ lib/stego/src/stego.c
@@ -92,7 +92,7 @@ void *decode(void* arg) {
     Bitmap *image = read_bitmap(image_source, &error_message);
     FILE *text_file = _open_file(text_destination, "w");
     message_length = _get_length(image->data);
-    for (int index = 4; index < message_length * 8; index++) {
+    for (int index = 4; index < image->header.image_size_bytes; index++) {
         buff = buff | ((image->data[index] & mask) << ((index - 4) % 8));
         if (((index - 4) % 8) > 6) {
             fputc(buff, text_file);
